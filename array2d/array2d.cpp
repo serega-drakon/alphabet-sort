5d3cfded13490be9f3076c53bc1f46f9e4e5b08b
serega.drakon <megaserega228@gmail.com>
2022-08-11 06:51:14 +0500

    Добавьте файлы проекта.

---
+++ array2d/array2d.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+
+#define READ 0
+#define WRITE 1
+#define MAX_X 3
+#define MAX_Y 3
+
+int array_2d(int x, int y, const _Bool mode, const int value);
+void swapY(int y1, int y2);
+
+main() { /* test */
+	for (int i = 0; i < MAX_Y; i++)
+		for (int j = 0; j < MAX_X; j++)
+			array_2d(j, i, WRITE, getchar());
+	for (int i = 0; i < MAX_Y; i++)
+		for (int j = 0; j < MAX_X; j++)
+			printf("%c", array_2d(j, i, READ, 0));
+
+}
+
+int static arr2d[MAX_X * MAX_Y];
+
+//WORKS!
+int array_2d(int x, int y, const _Bool mode, const int value) {
+	if (mode) { //write
+		if (x < MAX_X && y < MAX_Y) {
+			arr2d[y * MAX_X + x] = value;
+			return 0;
+		}
+		else
+			printf("arr..2d: error this element of arrange does not exist: x - %d, y - %d\n", x, y);
+	}
+	else { //read
+		if (x < MAX_X && y < MAX_Y)
+			return arr2d[y * MAX_X + x];
+		else
+			printf("arr..2d: error this element of arrange does not exist: x - %d, y - %d\n", x, y);
+	}
+	return -1;
+}
+
+void swapY(int y1, int y2) {
+
+}
---
+++ getline and fprint/getline and fprint.cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+#define MAXLINE 100
+
+int getline(int s[], int lim, FILE* in);
+void fprint(char s[], FILE* out);
+
+main() {
+	SetConsoleCP(1251); //localization
+	SetConsoleOutputCP(1251); // encoding of txt must be ANCI !!!!
+
+	FILE* infile;
+	FILE* outfile;
+	infile = fopen("C:\\Users\\slepy\\source\\repos\\alphabet sort\\getline_2d\\in.txt", "r");
+	outfile = fopen("C:\\Users\\slepy\\source\\repos\\alphabet sort\\getline_2d\\out.txt", "w");
+
+	int line[MAXLINE];
+	printf("%d\n", getline(line, MAXLINE, infile));
+	int i;
+	for (i = 0; i < MAXLINE && line[i] != '\0'; i++)
+		printf("%c", line[i]);
+
+	fprint(line, outfile);
+
+	fclose(outfile);
+	fclose(infile);
+}
+
+int getline(int s[], int lim, FILE* in) { //WORKS!
+	int i; int c;
+	for (i = 0; i < lim - 1 && (c = getc(in)) != '\n' && c != EOF; i++)
+		s[i] = c;
+	if (c == '\n')
+		s[i++] = '\n';
+	s[i] = '\0';
+	return i;
+}
+
+void fprint(int s[], FILE* out) { //WORKS! 
+	for (int i = 0; s[i] != '\0'; i++)
+		putc(s[i], out);
+}

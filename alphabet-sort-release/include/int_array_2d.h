0757a4ee890dcafae33a070eacd1012edc2707a4
serega-drakon <megaserega228@gmail.com>
2022-09-14 23:22:46 +0300

    Немного изменил структуру кода и обновил кучу функций, избавился от глобальных переменных

--- alphabet-sort-release/include/int_array_2d.h
+++ alphabet-sort-release/include/int_array_2d.h
@@ -12,7 +12,7 @@
 #define READ 0
 #define WRITE 1
 
-         //UPDATE
+  //UPDATE: вместо глобальной переменной сделал основу и "обертки"
 int arr2d_Main(int flag, int x, int y, int value){
     static int arr2d[MAX_X * MAX_Y];
     switch(flag){
@@ -24,23 +24,12 @@ int arr2d_Main(int flag, int x, int y, int value){
     }
 }
 
-int nextFreeY(int flag, int value){
-    static int nextfreeY_2d = 0;
-    switch(flag){
-        case READ:
-            return nextfreeY_2d;
-        case WRITE:
-            nextfreeY_2d = value;
-            return value;
-    }
-}
-
-//read        //UPDATE
+//read        //UPDATE: обертка
 int array_2d_r(int x, int y) {
     return arr2d_Main(READ, x, y, 0);
 }
 
-//write        //UPDATE
+//write        //UPDATE: обертка
 int array_2d_w(int x, int y, int value) {
     return arr2d_Main(WRITE, x, y, value);
 }
@@ -69,55 +58,22 @@ void copyfromstr(int y, int s[]) {
     s[x] = '\0';
 }
 
-int nextfreeY_2d = 0;
-
-// можно загрузиться сразу с нескольких файлов)
-void loadFromFile2d(FILE* in) {
+// можно загрузиться сразу с нескольких файлов)    //UPDATE: перешел к ссылке вместо глобальной переменной
+void loadFromFile2d(FILE* in, int *nextfreeY_2d) {
 	int line[ MAX_X ];
-	extern int nextfreeY_2d;
 	int x;
-	for (; _getline(line, MAX_X, in) > 0 && nextfreeY_2d < MAX_Y; nextfreeY_2d++)
-		copyfromstr(nextfreeY_2d, line);
+	for (; getline_(line, MAX_X, in) > 0 && (*nextfreeY_2d) < MAX_Y; (*nextfreeY_2d)++)
+		copyfromstr(*nextfreeY_2d, line);
+    if((*nextfreeY_2d) == MAX_Y && getline_(line, MAX_X, in) > 0)
+        printf("error: массив переполнен, но программа отсортирует все что может\n");
 }
 
-void saveToFile2d(FILE* out) {
-	extern int nextfreeY_2d;
+void saveToFile2d(FILE* out, int *nextfreeY_2d) {      //UPDATE
 	int line[ MAX_X ];
-	for (int y = 0; y < nextfreeY_2d; y++) {
+	for (int y = 0; y < *nextfreeY_2d; y++) {
 		copytostr(y, line);
 		fprint(line, out);
 	}
-}
-
-void reset_Save2d(void) {
-	extern int nextfreeY_2d;
-	nextfreeY_2d = 0;
-}
-
-                     //UPDATE
-//returns 0 if strings are the same
-//1 if first is higher
-//2 if second is higher
-int alphabetRUY(int y1, int s[]) //works with ASCII
-{
-	int st1, st2;
-	int i = 0, j = 0;
-	do {
-		//search for next letter
-		for (; (st1 = isletter(array_2d_r(i, y1))) != 3 && st1 != 4 && st1 != -1; i++)
-			;
-		for (; (st2 = isletter(s[j])) != 3 && st2 != 4 && st2 != -1; j++)
-			;
-        //"alphabetization"
-        st1 = alphabetPosRu(array_2d_r(i, y1));
-        st2 = alphabetPosRu(s[j]);
-		//comparison
-		if (st1 < st2)
-			return 1;
-		else if (st2 < st1)
-			return 2;
-	} while (st1 != (255 - 224 + 11) && st2 != (255 - 224 + 11) && i++ < MAX_X && j++ < MAX_X);
-	return 0;
 }
 
 #endif
--- alphabet-sort-release/main.c
+++ alphabet-sort-release/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "include/int_array_2d.h"
 
+int alphabetRUY(int y1, int s[]);
+
 int main(void) {
 	FILE* in;
 	FILE* out;
@@ -15,25 +17,50 @@ int main(void) {
         printf("error: there are no main_out.txt");
         return -2;
     }
-	loadFromFile2d(in);
+
+    int nextfreeY_2d = 0;//следующая свободная позиция в массиве (нужна для savetofile())
+	loadFromFile2d(in, &nextfreeY_2d);
 
 	//shell's sort algorithm
-	extern int nextfreeY_2d;
 	int gap, i, j;
     int buf[ MAX_X ];
 	for(gap = nextfreeY_2d / 2; gap > 0; gap /= 2)
 		for (i = gap; i < nextfreeY_2d; i++) {
-			//copyY(i, nextfreeY_2d);
             copytostr(i, buf);
 			for (j = i - gap; j >= 0 && alphabetRUY(j, buf) == 2; j -= gap)
 				copyY(j, j + gap);
 			if(j + gap != i)
-				//copyY(nextfreeY_2d,j + gap);
                 copyfromstr(j + gap, buf);
 		}
 
-	saveToFile2d(out);
+	saveToFile2d(out, &nextfreeY_2d);
 	fclose(in);
 	fclose(out);
+    return 0;
+}
+
+//UPDATE: сделал код более компактным исп. новую функцию
+//returns 0 if strings are the same
+//1 if first is higher
+//2 if second is higher
+int alphabetRUY(int y1, int s[]) //works with ASCII
+{
+    int st1, st2;
+    int i = 0, j = 0;
+    do {
+        //search for next letter
+        for (; (st1 = isletter(array_2d_r(i, y1))) != 3 && st1 != 4 && st1 != -1; i++)
+            ;
+        for (; (st2 = isletter(s[j])) != 3 && st2 != 4 && st2 != -1; j++)
+            ;
+        //"alphabetization"
+        st1 = alphabetPosRu(array_2d_r(i, y1));
+        st2 = alphabetPosRu(s[j]);
+        //comparison
+        if (st1 < st2)
+            return 1;
+        else if (st2 < st1)
+            return 2;
+    } while (st1 != (255 - 224 + 11) && st2 != (255 - 224 + 11) && i++ < MAX_X && j++ < MAX_X);
     return 0;
 }
